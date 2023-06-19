c5c1f37e20fece65490649a14fe88710da6ee808
Sandy <sandeep.malviya@stimveda.com>
2023-06-19 17:52:55 +0530

    Changes

--- windows/quick_blue_windows_plugin.cpp
+++ windows/quick_blue_windows_plugin.cpp
@@ -24,6 +24,8 @@
 #include <algorithm>
 #include <iomanip>
 
+#include <guiddef.h>
+
 #define GUID_FORMAT "%08x-%04hx-%04hx-%02hhx%02hhx-%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx"
 #define GUID_ARG(guid) guid.Data1, guid.Data2, guid.Data3, guid.Data4[0], guid.Data4[1], guid.Data4[2], guid.Data4[3], guid.Data4[4], guid.Data4[5], guid.Data4[6], guid.Data4[7]
 
